ebbc8ed104981d78b6a36b073f749cd7c63a5fd7
Angelo Vazquez <angelovazquez@Angelos-MacBook-Pro.local>
2021-01-11 17:03:38 -0600

    server fix

--- modules/app/app.js
+++ modules/app/app.js
@@ -8,8 +8,4 @@ app.use(cors())
 app.use('/people', require('../people/people.router'))
 app.use('/pets', require('../pets/pets.router'))
 
-app.get('/', (req, res, next) => {
-    res.send ('Petful Server');
-})
-
 module.exports = app
